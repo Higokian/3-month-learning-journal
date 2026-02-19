ef9752b29ad3ebfd60cb3239fe8003ea7816657f
Jason <jason_ritter@outlook.com>
2026-02-19 16:56:25 -0600

    Get html elements by id

---
+++ practice/reconstruction-week/tip-calculator/script.js
@@ -0,0 +1,23 @@
+// 1.) Get elements by id
+const element = (id) => document.getElementById(id);
+
+const elements = {
+  billAmount: element("billAmount"),
+  tipPercentage: element("tipPercentage"),
+  numPeople: element("numPeople"),
+  calculateBtn: element("calculateBtn"),
+  results: element("results"),
+  tipAmount: element("tipAmount"),
+  totalBill: element("totalBill"),
+  perPerson: element("perPerson"),
+}
+
+const {billAmount, tipPercentage, numPeople, calculateBtn, results, tipAmount, totalBill, perPerson} = elements;
+
+// 2.) Func - calcTip
+
+// 3.) Func - calcNumPeople
+
+// 4.) Validation
+
+// 5.) Event listeners
